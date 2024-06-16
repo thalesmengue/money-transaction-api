eac20ab7052b920e574f03b6b1b65bfd1dbb7066
Thales Machado <thalesmmachado@gmail.com>
2024-06-15 23:00:48 -0300

    fix: adjust the application feature tests

--- tests/Feature/UserControllerTest.php
+++ tests/Feature/UserControllerTest.php
@@ -13,22 +13,18 @@ class UserControllerTest extends TestCase
 {
     use RefreshDatabase;
 
-    public function test_authenticated_user_can_find_a_user(): void
+    public function test_can_find_a_user(): void
     {
         $user = User::factory()->create();
-
-        Passport::actingAs($user);
 
         $response = $this->get(route('user.show', $user->id));
 
         $response->assertStatus(Response::HTTP_FOUND);
     }
 
-    public function test_authenticated_user_can_delete_a_user(): void
+    public function test_can_delete_a_user(): void
     {
         $user = User::factory()->create();
-
-        Passport::actingAs($user);
 
         $response = $this->delete(route('user.destroy', $user->id));
 
